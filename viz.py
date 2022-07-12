6d55bf8f763a61e63bab963d0c2b11fd7ad840a8
Haziq958 <haziqkhan.hassanzai@gmail.com>
2022-07-12 16:52:58 +0200

    heatMap for coarse labels according to superclass

--- viz.py
+++ viz.py
@@ -1,5 +1,7 @@
 '''Train PS-KD: learning with PyTorch.'''
 from __future__ import print_function
+import configparser
+from matplotlib.contour import ContourSet
 
 #----------------------------------------------------
 #  Pytorch
@@ -8,6 +10,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #--------------
 #  Datalodader
@@ -51,10 +55,39 @@ def parse_args():
 #----------------------------------------------------
 C = Colorer.instance()
 
+
+#----------------------------------------------------
+# returns the superclass and how many indexes before it were in the same class
+#----------------------------------------------------
+
+def sparse2coarse(targets):
+    """Convert Pytorch CIFAR100 sparse targets to coarse targets.
+    Usage:
+        trainset = torchvision.datasets.CIFAR100(path)
+        trainset.targets = sparse2coarse(trainset.targets)
+    """
+    coarse_labels = np.array([ 4,  1, 14,  8,  0,  6,  7,  7, 18,  3,  
+                               3, 14,  9, 18,  7, 11,  3,  9,  7, 11,
+                               6, 11,  5, 10,  7,  6, 13, 15,  3, 15,  
+                               0, 11,  1, 10, 12, 14, 16,  9, 11,  5, 
+                               5, 19,  8,  8, 15, 13, 14, 17, 18, 10, 
+                               16, 4, 17,  4,  2,  0, 17,  4, 18, 17, 
+                               10, 3,  2, 12, 12, 16, 12,  1,  9, 19,  
+                               2, 10,  0,  1, 16, 12,  9, 13, 15, 13, 
+                              16, 19,  2,  4,  6, 19,  5,  5,  8, 19, 
+                              18,  1,  2, 15,  6,  0, 17,  8, 14, 13])
+    j=0
+    for i in range(targets+1):
+       
+        if coarse_labels[i]== coarse_labels[targets]:
+            j= j+1
+            
+    return coarse_labels[targets], j-1
+
 def main():
     parser, args = parse_args()
-    config_file = os.path.join(args.experiment_dir, 'config/config.json')
-    model_dir = os.path.join(args.experiment_dir, 'model')
+    config_file = os.path.join(args.experiment_dir, 'C:/Users/haziq/Desktop/DL Project/ProjectFinal/PS-KD-Pytorch/exptsnew/cifar100_SmallResNet__2022-7-9-16-47-42/config/config.json')
+    model_dir = os.path.join(args.experiment_dir, 'C:/Users/haziq/Desktop/DL Project/ProjectFinal/PS-KD-Pytorch/exptsnew/cifar100_SmallResNet__2022-7-9-16-47-42/model')
     assert os.path.exists(config_file), "config file path incorrect"
     assert os.path.exists(model_dir), "model directory path incorrect"
     
@@ -78,11 +111,37 @@ def main():
         net.load_state_dict(checkpoint['net'])
         learnable_params = net.learnable_params.weight.data # tensor of shape = [num_classes, 512]
         learnable_params = learnable_params.clone().detach().cpu().numpy() # nparray of shape = [num_classes, 512]
+        print(learnable_params.shape)
         similarity_matrix = learnable_params @ learnable_params.T
         sim_matrices.append(similarity_matrix)
+        
+        #print(similarity_matrix)
+    
     
     sim_matrices = np.array(sim_matrices)
     np.save(open(os.path.join(model_dir,'learnable_parameters_similarity.npy'),'wb'),sim_matrices)
+   
+    print(len(sim_matrices))
+    
+    coarse_sim_matrices = np.zeros((100,100))
+
+   
+    print(coarse_sim_matrices.shape)
+    
+    def heatMap(sim_matrices):
+        #for i in range(len(sim_matrices)):
+            for x in range(100):
+                for j in range(100):
+                    coarse_sim_matrices[sparse2coarse(x)[0]*5 + sparse2coarse(x)[1]][sparse2coarse(j)[0]*5 + sparse2coarse(j)[1]]= sim_matrices[2][x][j]
+            #coarse_sim_matrices.append(coarse_sim_matrices)
+            ax = sns.heatmap(coarse_sim_matrices)
+            plt.show()
+            return
+      
+    heatMap(sim_matrices)
+    
+    # ax = sns.heatmap(sim_matrices[2])
+    # plt.show()
 
 if __name__ == '__main__':
     main()
